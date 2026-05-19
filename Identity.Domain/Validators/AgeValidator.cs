96ba3bf1062dd0793020e0cf5f9bfbcd187fde80
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-19 19:38:13 +0200

    chore: bump down min age

--- Identity.Domain/Validators/AgeValidator.cs
+++ Identity.Domain/Validators/AgeValidator.cs
@@ -7,6 +7,6 @@ public class AgeValidator : AbstractValidator<DateOnly>
     public AgeValidator()
     {
         RuleFor(x => x).NotEmpty().WithMessage("Age cannot be empty");
-        RuleFor(x => x).LessThan(DateOnly.FromDateTime(DateTime.Now.AddYears(-14))).WithMessage("Age must be greater than 14");
+        RuleFor(x => x).LessThan(DateOnly.FromDateTime(DateTime.Now.AddYears(-13))).WithMessage("Age must be greater than 13");
     }
 }

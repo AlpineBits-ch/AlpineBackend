61fbce4ca3814b752f1a3c6253b8453ebbe0706c
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-20 20:08:58 +0200

    feat: add pub and priv key to federation config

---
+++ Federation.Application/Dtos/Events/FederationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Federation.Application.Dtos.Events;
+
+public abstract class FederationEvent
+{
+    
+}
+
+
+// mock event for now
+public class MessageReceived : FederationEvent
+{
+    public string Message { get; set; }   
+}
--- Federation.Application/Dtos/Response/FederationDocumentResponse.cs
+++ Federation.Application/Dtos/Response/FederationDocumentResponse.cs
@@ -5,9 +5,10 @@ namespace Federation.Application.Dtos.Response;
 public class FederationDocumentResponse
 {
     public string Instance { get; init; } = Env.GeneralConfiguration.InstanceUrl;
-    public string InstanceName { get; init; } = Env.GeneralConfiguration.InstanceName;
-    public string Version { get; init; } = Env.GeneralConfiguration.Version;
+    public string InstanceName { get; init; } = Env.Federation.InstanceName;
+    public string Version { get; init; } = Env.Federation.Version;
     public string FederationApi { get; init; } = $"{Env.GeneralConfiguration.InstanceUrl}/federation/v1";
+    public byte[] PublicKey { get; init; } = Env.Federation.PublicKey;
     public FederationDocumentCapabilities Capabilities { get; init; } = new();
 }
 

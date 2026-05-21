ca4296eabf6929057c6893ac6e1b4cc9de4b2b44
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-21 21:32:29 +0200

    feat: add host key

--- Federation.Application/Endpoints/FederationEndpoint.cs
+++ Federation.Application/Endpoints/FederationEndpoint.cs
@@ -1,7 +1,10 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using Federation.Application.Dtos.Events;
+using Federation.Domain.Events;
+using Federation.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Wolverine.Http;
 
 namespace Federation.Application.Endpoints;
@@ -9,9 +12,27 @@ namespace Federation.Application.Endpoints;
 public class FederationEndpoint 
 {
     [WolverinePost("api/v1/federation/events")]
-    public async Task<(IResult, FederationEvent)> EventAsync([FromBody] FederationEvent @event, [NotBody] ILogger<FederationEndpoint> logger)
+    public async Task<(IResult, FederationEvent?)> EventAsync([FromBody] SignedFederationEvent @event, [NotBody] ILogger<FederationEndpoint> logger, MicroserviceContext context)
     {
         logger.LogInformation("Got event {event}", JsonSerializer.Serialize(@event));
-        return (Results.Ok(), @event);
+        
+        var federatedSystem = await context.FederationInstances.FirstOrDefaultAsync(i => i.Host == @event.Payload.Host);
+        
+        if(federatedSystem is null) 
+        {
+            logger.LogWarning("No federated system found for public key {key}", @event.PublicKey);
+            return (Results.BadRequest(), null);
+        }
+        if (federatedSystem.Status != FederationStatus.Active)
+        {
+            logger.LogWarning("Federation instance {host} is not active", federatedSystem.Host);
+            return (Results.Forbid(), null);
+        }
+        if (!@event.IsValid(federatedSystem))
+        {
+            return (Results.BadRequest(), null);
+        }
+        
+        return (Results.Ok(), @event.Payload);
     }
 }

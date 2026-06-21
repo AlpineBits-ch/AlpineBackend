2920c6f9630a8a12899ebc14460985d5602f24cf
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-21 10:40:49 +0200

    feat: simplify s3 storage registration

--- Social.Infrastructure/SocialInfrastructure.cs
+++ Social.Infrastructure/SocialInfrastructure.cs
@@ -20,42 +20,9 @@ public static class SocialInfrastructure
     {
         try
         {
-            
-            var storageConfig = Env.StorageConfiguration;
 
-            var s3Config = new AmazonS3Config
-            {
-      
-                ForcePathStyle = true 
-            };
-
-            if (storageConfig.UseServiceUrl)
-            {
-                s3Config.ServiceURL = storageConfig.ServiceUrl;
-            }
-            else
-            {
-               
-                string region = Env.StorageConfiguration.Region;
-                s3Config.RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region);
-            }
-
-            var credentials = new Amazon.Runtime.BasicAWSCredentials(
-                storageConfig.AccessKey,
-                storageConfig.SecretKey
-            );
-
-            services.AddSingleton<IAmazonS3>(new AmazonS3Client(credentials, s3Config));
-            
-            
-            var googleServiceAccountJsonBase64 = Env.GoogleServiceAccountJsonBase64;
-            var credsJson = Convert.FromBase64String(googleServiceAccountJsonBase64);
-            
-                
-            using var  ms = new MemoryStream(credsJson);
-       
-            
-            
+            services.AddS3Storage();
+    
         }
         catch (Exception e)
         {

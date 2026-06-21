8e023774f1f672d98f854dc60f1cb33c7ff95af0
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-21 07:36:31 +0200

    fix: user type

---
+++ Identity.Infrastructure/Migrations/20260621053536_UserTypeIsNowEnum.cs
@@ -0,0 +1,70 @@
+﻿using Identity.Domain.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTypeIsNowEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:age_vertification_level", "ai_estimation,goverment_id,none,self_declaration")
+                .Annotation("Npgsql:Enum:device_status", "active,removed")
+                .Annotation("Npgsql:Enum:device_type", "desktop,mobile,web")
+                .Annotation("Npgsql:Enum:direct_message_settings", "allow_all,filter_all,filter_non_friends")
+                .Annotation("Npgsql:Enum:privacy_settings", "allow_data_collection,allow_data_use_for_personalization,allow_voice_recorded_in_clips,none")
+                .Annotation("Npgsql:Enum:theme", "dark,light,midnight,system")
+                .Annotation("Npgsql:Enum:user_status", "active,banned,inactive")
+                .Annotation("Npgsql:Enum:user_type", "admin,default,moderator")
+                .OldAnnotation("Npgsql:Enum:age_vertification_level", "ai_estimation,goverment_id,none,self_declaration")
+                .OldAnnotation("Npgsql:Enum:device_status", "active,removed")
+                .OldAnnotation("Npgsql:Enum:device_type", "desktop,mobile,web")
+                .OldAnnotation("Npgsql:Enum:direct_message_settings", "allow_all,filter_all,filter_non_friends")
+                .OldAnnotation("Npgsql:Enum:privacy_settings", "allow_data_collection,allow_data_use_for_personalization,allow_voice_recorded_in_clips,none")
+                .OldAnnotation("Npgsql:Enum:theme", "dark,light,midnight,system")
+                .OldAnnotation("Npgsql:Enum:user_status", "active,banned,inactive");
+
+            
+            migrationBuilder.DropColumn(table: "asp_net_users", name: "user_type");
+            
+            migrationBuilder.AddColumn<UserType>(
+                name: "user_type",
+                table: "asp_net_users",
+                type: "user_type",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:age_vertification_level", "ai_estimation,goverment_id,none,self_declaration")
+                .Annotation("Npgsql:Enum:device_status", "active,removed")
+                .Annotation("Npgsql:Enum:device_type", "desktop,mobile,web")
+                .Annotation("Npgsql:Enum:direct_message_settings", "allow_all,filter_all,filter_non_friends")
+                .Annotation("Npgsql:Enum:privacy_settings", "allow_data_collection,allow_data_use_for_personalization,allow_voice_recorded_in_clips,none")
+                .Annotation("Npgsql:Enum:theme", "dark,light,midnight,system")
+                .Annotation("Npgsql:Enum:user_status", "active,banned,inactive")
+                .OldAnnotation("Npgsql:Enum:age_vertification_level", "ai_estimation,goverment_id,none,self_declaration")
+                .OldAnnotation("Npgsql:Enum:device_status", "active,removed")
+                .OldAnnotation("Npgsql:Enum:device_type", "desktop,mobile,web")
+                .OldAnnotation("Npgsql:Enum:direct_message_settings", "allow_all,filter_all,filter_non_friends")
+                .OldAnnotation("Npgsql:Enum:privacy_settings", "allow_data_collection,allow_data_use_for_personalization,allow_voice_recorded_in_clips,none")
+                .OldAnnotation("Npgsql:Enum:theme", "dark,light,midnight,system")
+                .OldAnnotation("Npgsql:Enum:user_status", "active,banned,inactive")
+                .OldAnnotation("Npgsql:Enum:user_type", "admin,default,moderator");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "user_type",
+                table: "asp_net_users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(UserType),
+                oldType: "user_type");
+        }
+    }
+}

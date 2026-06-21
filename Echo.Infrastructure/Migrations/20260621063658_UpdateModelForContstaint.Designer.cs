d661eec7b25dc83afbc37def2296922a6c4e6453
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-21 08:37:13 +0200

    fix: cstr

--- Echo.Infrastructure/Migrations/20260621063658_UpdateModelForContstaint.Designer.cs
+++ Echo.Infrastructure/Migrations/20260621063658_UpdateModelForContstaint.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Echo.Persistence.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Echo.Persistence.Migrations
 {
     [DbContext(typeof(MicroserviceContext))]
-    partial class MicroserviceContextModelSnapshot : ModelSnapshot
+    [Migration("20260621063658_UpdateModelForContstaint")]
+    partial class UpdateModelForContstaint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

6b0f4d8f5ed062688e378d74a4333f0f9870cf69
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-21 10:40:49 +0200

    feat: simplify s3 storage registration

---
+++ AppEnvironment/StorageInstance.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.S3;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppEnvironment;
+
+public static class StorageInstance
+{
+    public static IServiceCollection AddS3Storage(this IServiceCollection services)
+    {
+        var storageConfig = Env.StorageConfiguration;
+
+        var s3Config = new AmazonS3Config
+        {
+      
+            ForcePathStyle = true 
+        };
+
+        if (storageConfig.UseServiceUrl)
+        {
+            s3Config.ServiceURL = storageConfig.ServiceUrl;
+        }
+        else
+        {
+               
+            string region = Env.StorageConfiguration.Region;
+            s3Config.RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region);
+        }
+
+        var credentials = new Amazon.Runtime.BasicAWSCredentials(
+            storageConfig.AccessKey,
+            storageConfig.SecretKey
+        );
+
+        services.AddSingleton<IAmazonS3>(new AmazonS3Client(credentials, s3Config));
+        return services;
+    }
+}
--- Guild.Infrastructure/GuildInfrastructure.cs
+++ Guild.Infrastructure/GuildInfrastructure.cs
@@ -36,31 +36,8 @@ public static class GuildInfrastructure
         try
         {
              
-            var storageConfig = Env.StorageConfiguration;
+            services.AddS3Storage();
 
-            var s3Config = new AmazonS3Config
-            {
-      
-                ForcePathStyle = true 
-            };
-
-            if (storageConfig.UseServiceUrl)
-            {
-                s3Config.ServiceURL = storageConfig.ServiceUrl;
-            }
-            else
-            {
-               
-                string region = Env.StorageConfiguration.Region;
-                s3Config.RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region);
-            }
-
-            var credentials = new Amazon.Runtime.BasicAWSCredentials(
-                storageConfig.AccessKey,
-                storageConfig.SecretKey
-            );
-
-            services.AddSingleton<IAmazonS3>(new AmazonS3Client(credentials, s3Config));
             
             var googleServiceAccountJsonBase64 = Env.GoogleServiceAccountJsonBase64;
             var googleServiceAccountJson = Convert.FromBase64String(googleServiceAccountJsonBase64);

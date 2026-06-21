f2822edd6444b2adea62d666f20630a1177ca5d9
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-21 06:18:25 +0200

    feat: add configuration

---
+++ Echo.Infrastructure/Migrations/20260621041754_AddConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Echo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "echo_configurations",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    is_register_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    is_login_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    enforced_singleton = table.Column<int>(type: "integer", nullable: false, defaultValue: 1),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_echo_configurations", x => x.id);
+                    table.CheckConstraint("ck_single_row_enforcer", "[enforced_singleton] = 1");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_echo_configurations_enforced_singleton",
+                table: "echo_configurations",
+                column: "enforced_singleton",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "echo_configurations");
+        }
+    }
+}
--- Echo.Infrastructure/Persistance/MicroserviceContext.cs
+++ Echo.Infrastructure/Persistance/MicroserviceContext.cs
@@ -1,8 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Runtime.CompilerServices;
+using AppEnvironment;
+using Echo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Echo.Persistence.Persistance;
 
 public class MicroserviceContext : DbContext
 {
+    public DbSet<EchoConfiguration> EchoConfigurations { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var env = Env.Database;
+
+        optionsBuilder.UseNpgsql(env.ConnectionString(), options =>
+        {
+         
+        }).UseSnakeCaseNamingConvention();
+    }
     
+    public MicroserviceContext(DbContextOptions<MicroserviceContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<EchoConfiguration>(builder =>
+        {
+            builder.Property(x => x.EnforcedSingleton)
+                .HasDefaultValue(1);
+            builder.ToTable(table =>
+            {
+                table.HasCheckConstraint("ck_single_row_enforcer", "[enforced_singleton] = 1");
+            });
+            builder.HasIndex(x => x.EnforcedSingleton).IsUnique();
+        });
+
+    }
+
 }

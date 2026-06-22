9e3d47de73a9b7aaad53d06608ecf1a68ababd27
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-22 22:07:31 +0200

    feat: add federated events for message, and user joined leave

---
+++ Federation.Domain/Events/Federation/UserJoinedFederatedEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace Federation.Domain.Events.Federation;
+
+public class UserJoinedFederatedEvent : FederatedEvent
+{
+    public override string EventType => "m.channel.user.joined";
+    
+    public UserJoinedFederatedEvent(
+        EventId eventId, 
+        IEnumerable<EventId> prevEvents, 
+        long depth, 
+        string channelId, 
+        string senderId, 
+        DateTime originServerTimestamp) 
+        : base(eventId, prevEvents, depth, channelId, senderId, originServerTimestamp)
+    {
+    }
+}

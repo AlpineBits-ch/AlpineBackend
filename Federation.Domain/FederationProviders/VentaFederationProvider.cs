ec067d42e4156214cc35cd51d535cf4abcfe175f
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-23 18:48:27 +0200

    feat: add federation protocol provider

---
+++ Federation.Domain/FederationProviders/VentaFederationProvider.cs
@@ -0,0 +1,40 @@
+﻿using Federation.Domain.Events;
+
+namespace Federation.Domain.FederationProviders;
+
+public class VentaFederationProvider : IFederationProvider
+{
+    public FederationProtocolVersion ProtocolVersion { get; } = new FederationProtocolVersion("venta", 0, 1);
+
+    public Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ShutdownAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task SendMessageAsync(string channelId, byte[] message, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task JoinChannelAsync(string channelId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task LeaveChannelAsync(string channelId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<object> GetUserProfileAsync(string userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public event Action<FederatedEvent>? OnFederatedEventReceived;
+}

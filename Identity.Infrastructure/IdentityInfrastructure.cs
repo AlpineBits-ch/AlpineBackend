d1eba196bf8707cb353f66077e62f45694776eac
Dominic Järmann <dominic.jaermann@golog.ch>
2026-06-24 12:29:09 +0200

    chore: remove unneeded wolverine setup gates

--- Identity.Infrastructure/IdentityInfrastructure.cs
+++ Identity.Infrastructure/IdentityInfrastructure.cs
@@ -10,12 +10,7 @@ public static class IdentityInfrastructure
     public static void UseInfrastructure(this IApplicationBuilder builder)
     {
         var scope = builder.ApplicationServices.CreateScope();
-        var db = scope.ServiceProvider.GetRequiredService<MicroserviceContext>();
-
-        if (db.Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
-        {
-            db.Database.Migrate(); // or whatever line 14 is doing
-        }
+        scope.ServiceProvider.GetRequiredService<MicroserviceContext>().Database.Migrate();
     }
 
     public static void AddInfrastructure(this IServiceCollection services)

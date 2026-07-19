936d11ca68edea732465cc7b6ea7ce80e22926c1
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-19 22:34:36 +0200

    chore: presence service

---
+++ Isle.Application/Chat/PresenceService.cs
@@ -0,0 +1,20 @@
+﻿using IsleBridge.Sdk;
+
+namespace Isle.Api.Chat;
+
+public class PresenceService(IBridgeClient bridgeClient, ILogger<PresenceService> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            var players = await bridgeClient.GetPlayersAsync(stoppingToken);
+            foreach (var player in players.Players)
+            {
+                logger.LogInformation($"Player {player} is online");
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+        
+    }
+}

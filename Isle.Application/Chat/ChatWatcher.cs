30da23496a8cc4e0ae6fae32459f142798d44001
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-19 19:58:30 +0200

    chore: add more event logs

--- Isle.Application/Chat/ChatWatcher.cs
+++ Isle.Application/Chat/ChatWatcher.cs
@@ -1,5 +1,6 @@
 ﻿using IsleBridge.Sdk;
 using IsleBridge.Sdk.Models;
+using Microsoft.VisualBasic;
 
 namespace Isle.Api.Chat;
 
@@ -10,6 +11,22 @@ public class ChatWatcher(IChatStream chat, IEventStream events, ILogger<ChatWatc
         await foreach (ChatMessage msg in chat.StreamAsync(ct))
         {
             logger.LogInformation("{UserName} with steam {Steam} wrote in chat {Text}", msg.Name, msg.Steam, msg.Text);
+        }
+        
+        await foreach (GameEvent msg in events.StreamAsync(ct))
+        {
+            if (msg.Kind == EventKind.Death)
+            {
+                logger.LogInformation("Player {Name} died", msg.Steam);
+            }
+            if(msg.Kind == EventKind.Join)
+            {
+                logger.LogInformation("Player {Name} joined", msg.Steam);
+            }
+            if(msg.Kind == EventKind.Leave)
+            {
+                logger.LogInformation("Player {Name} left", msg.Steam);
+            }
         }
     }
 }

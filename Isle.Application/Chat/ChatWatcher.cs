38be2725041f65dfc6206d270dbb1a639ad32439
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-19 17:55:13 +0200

    chore: update chat log

--- Isle.Application/Chat/ChatWatcher.cs
+++ Isle.Application/Chat/ChatWatcher.cs
@@ -3,13 +3,13 @@ using IsleBridge.Sdk.Models;
 
 namespace Isle.Api.Chat;
 
-public class ChatWatcher(IChatStream chat, IEventStream events) : BackgroundService
+public class ChatWatcher(IChatStream chat, IEventStream events, ILogger<ChatWatcher> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken ct)
     {
         await foreach (ChatMessage msg in chat.StreamAsync(ct))
         {
-            Console.WriteLine(msg);
+            logger.LogInformation("{UserName} with steam {Steam} wrote in chat {Text}", msg.Name, msg.Steam, msg.Text);
         }
     }
 }

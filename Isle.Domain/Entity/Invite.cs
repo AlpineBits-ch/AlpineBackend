47c7602a7b46dd3808b6942148412481fa665872
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-19 22:03:06 +0200

    feat: listen to events and chat

---
+++ Isle.Domain/Entity/Invite.cs
@@ -0,0 +1,38 @@
+﻿using Isle.Domain.Aggregates;
+using Persistence;
+
+namespace Isle.Domain.Entity;
+
+public class CreateInviteParams
+{
+    public string ServerId { get; set; }
+    public string SenderPlayerId { get; set; }
+    public string ReceiverPlayerId { get; set; }
+}
+
+public class Invite : BaseEntity<Invite>, IPrefixedEntity
+{
+    public static string Prefix { get; } = "invite";
+    public virtual IsleServer Server { get; set; }
+    public string ServerId { get; set; }
+    public virtual Player SenderPlayer { get; set; }
+    public string SenderPlayerId { get; set; }
+    public virtual Player ReceiverPlayer { get; set; }
+    public string ReceiverPlayerId { get; set; }
+
+    public static Invite Create(CreateInviteParams parameters)
+    {
+        var id = Invite.GenerateId();
+        var date = DateTime.UtcNow;
+
+        return new Invite()
+        {
+            Id = id,
+            CreatedAt = date,
+            UpdatedAt = date,
+            ServerId = parameters.ServerId,
+            SenderPlayerId = parameters.SenderPlayerId,
+            ReceiverPlayerId = parameters.ReceiverPlayerId,
+        };
+    }
+}
---
+++ Isle.Domain/Exceptions/StorageFullException.cs
@@ -0,0 +1,7 @@
+﻿namespace Isle.Domain.Exceptions;
+
+public class StorageFullException(string storageId, int maxSlots) : Exception
+{
+    public string StorageId { get; set; } = storageId;
+    public int MaxSlots { get; set; } = maxSlots;
+}

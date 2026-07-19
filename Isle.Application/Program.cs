4e8564d63f6e85e0b912da0aab6768c5d152887e
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-19 17:55:27 +0200

    chore: update chat log

--- Isle.Application/Program.cs
+++ Isle.Application/Program.cs
@@ -4,7 +4,7 @@ using IsleBridge.Sdk;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGracefulShutdownHealthCheck();
-
+builder.Services.AddLogging();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();

58ed2d2ada6fbd0450b5bb5f4556d499ed61889c
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 20:24:39 +0200

    feat: support both directional registration

---
+++ Identity.Application/Handlers/IslePlayerHandler.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Contracts.Bus.Events;
+using Identity.Infrastructure.Persistence;
+using Isle.Contracts.Events;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Application.Handlers;
+
+public class IslePlayerHandler(MicroserviceContext context)
+{
+    public async Task<SteamLinkedEvent?> HandleIslePlayerCreated(PlayerCreatedEvent @event)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(p => p.SteamId == @event.SteamId);
+        if (user is not null)
+        {
+            return new SteamLinkedEvent()
+            {
+                UserId = user.Id,
+                SteamId = @event.SteamId,
+            };
+        }
+        return null;
+    }
+}

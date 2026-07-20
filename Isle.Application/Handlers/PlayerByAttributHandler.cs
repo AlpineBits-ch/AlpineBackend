ee886e548029341d4b646349f4835a0b99a1a11b
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 20:03:33 +0200

    feat: add player resoltion contract

---
+++ Isle.Application/Handlers/PlayerByAttributHandler.cs
@@ -0,0 +1,40 @@
+﻿using Isle.Contracts.Bus;
+using Isle.Contracts.Dtos;
+using Isle.Domain.Aggregates;
+using Isle.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using TheIsleEvrimaRconClient.Extensions.Models;
+
+namespace Isle.Api.Handlers;
+
+public class PlayerByAttributHandler(MicroserviceContext context)
+{
+    public async Task<GetPlayerBySteamIdResponse> Handle(GetPlayerBySteamIdRequest request, CancellationToken cancellationToken)
+    {
+        var player = await context.Players.FirstOrDefaultAsync(x => x.SteamId == request.SteamId, cancellationToken);
+        if(player == null) return new GetPlayerBySteamIdResponse();
+        
+        return new GetPlayerBySteamIdResponse { Player = FromPlayer(player) };
+    }
+    
+    public async Task<GetPlayerByUserIdResponse> Handle(GetPlayerByUserIdRequest request, CancellationToken cancellationToken)
+    {
+        var player = await context.Players.FirstOrDefaultAsync(x => x.UserId == request.UserId, cancellationToken);
+        if(player == null) return new GetPlayerByUserIdResponse();
+
+        return new GetPlayerByUserIdResponse { Player = FromPlayer(player) };
+    }
+
+    private PlayerDto FromPlayer(Player player)
+    {
+        return new PlayerDto()
+        {
+            Id = player.Id,
+            CreatedAt = player.CreatedAt,
+            UpdatedAt = player.UpdatedAt,
+            SteamId = player.SteamId,
+            Xp = player.Xp,
+            UserId = player.UserId
+        };
+    }
+}
---
+++ Isle.Contracts/Dtos/PlayerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Isle.Contracts.Dtos;
+
+public class PlayerDto
+{
+    public string Id {get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public string SteamId { get; set; }
+    public string? UserId { get; set; }
+    public long Xp { get; set; }
+}

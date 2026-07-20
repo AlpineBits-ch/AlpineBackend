546b3342f8f4551afb384fe752eb8824371036cd
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 12:56:07 +0200

    feat: create invite

--- Isle.Infrastructure/Persistence/MicroserviceContext.cs
+++ Isle.Infrastructure/Persistence/MicroserviceContext.cs
@@ -1,9 +1,48 @@
-﻿using Isle.Domain.Aggregates;
+﻿using AppEnvironment;
+using Isle.Domain.Aggregates;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace Isle.Infrastructure.Persistence;
 
 public class MicroserviceContext : DbContext
 {
     public DbSet<Player> Players { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var env = Env.Database;
+
+        optionsBuilder.UseNpgsql(env.ConnectionString(), options =>
+        {
+        
+        }).UseSnakeCaseNamingConvention();
+    }
+    public MicroserviceContext(DbContextOptions<MicroserviceContext> options) : base(options)
+    {
+        
+    }
+
+    
+    
+    public override int SaveChanges()
+    {
+        ChangeTracker.UpdateTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ChangeTracker.UpdateTimestamps();
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+    {
+        ChangeTracker.UpdateTimestamps();
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+    
 }

f5fadfac62a3b728c2ff9906b74fb9512c048b88
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 08:52:47 +0200

    yeet: log noise

--- Isle.Application/Chat/PresenceService.cs
+++ Isle.Application/Chat/PresenceService.cs
@@ -11,7 +11,7 @@ public class PresenceService(IBridgeClient bridgeClient, ILogger<PresenceService
             var players = await bridgeClient.GetPlayersAsync(stoppingToken);
             foreach (var player in players.Players)
             {
-                logger.LogInformation($"Player {player} is online");
+                //logger.LogInformation($"Player {player} is online");
             }
             await Task.Delay(1000, stoppingToken);
         }

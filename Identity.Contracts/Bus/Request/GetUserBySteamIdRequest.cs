9d5f30b98800d23b7b2e8ef7fa231fd79feaaa59
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 19:58:00 +0200

    fet: add get user by steam id handler

---
+++ Identity.Contracts/Bus/Request/GetUserBySteamIdRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Contracts.Bus.Request;
+
+public class GetUserBySteamIdRequest
+{
+    public string SteamId { get; set; }
+}
---
+++ Identity.Contracts/Bus/Response/GetUserBySteamIdResponse.cs
@@ -0,0 +1,8 @@
+﻿using Identity.Contracts.Dto.Response;
+
+namespace Identity.Contracts.Bus.Response;
+
+public class GetUserBySteamIdResponse
+{
+    public ApplicationUserDto? User { get; set; }
+}

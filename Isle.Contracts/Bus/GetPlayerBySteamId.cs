927455bf42fe9267297d608a98811d590426fa71
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-20 20:03:33 +0200

    feat: add player resoltion contract

---
+++ Isle.Contracts/Bus/GetPlayerBySteamId.cs
@@ -0,0 +1,24 @@
+﻿using Isle.Contracts.Dtos;
+
+namespace Isle.Contracts.Bus;
+
+public class GetPlayerBySteamIdRequest
+{
+    public string SteamId { get; set; }
+}
+
+public class GetPlayerBySteamIdResponse
+{
+    public PlayerDto? Player { get; set; }
+}
+
+
+public class GetPlayerByUserIdRequest
+{
+    public string UserId { get; set; }
+}
+
+public class GetPlayerByUserIdResponse
+{
+    public PlayerDto? Player { get; set; }
+}

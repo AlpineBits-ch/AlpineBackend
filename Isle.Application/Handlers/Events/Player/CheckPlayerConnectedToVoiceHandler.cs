19e0eef07a18a6639445839403ca37fdbc7731e5
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-21 23:09:12 +0200

    chore: stopp voice spam

--- Isle.Application/Handlers/Events/Player/CheckPlayerConnectedToVoiceHandler.cs
+++ Isle.Application/Handlers/Events/Player/CheckPlayerConnectedToVoiceHandler.cs
@@ -1,4 +1,5 @@
-﻿using Isle.Contracts.Events.Player;
+﻿using Isle.Api.Services;
+using Isle.Contracts.Events.Player;
 using IsleBridge.Sdk;
 using Wolverine;
 
@@ -17,8 +18,14 @@ public class CheckPlayerConnectedToVoiceHandler
         }.DelayedFor(TimeSpan.FromSeconds(60));
     }
     
-    public async Task<object> Handle(EnsurePlayerConnectedToVoiceEvent @event, IBridgeClient client)
+    public async Task<object> Handle(EnsurePlayerConnectedToVoiceEvent @event, IBridgeClient client, VoicePlayerRegistry registry)
     {
+
+        if (registry.TryGetPlayerId(@event.SteamId, out var playerId))
+        {
+            if (!string.IsNullOrWhiteSpace(playerId)) return null;
+        }
+        
         await client.DmAsync("Hi! Hope you are enjoying the game. Please make sure you are connected to voice. Download our client on https://venta.gg and link your steam!", @event.SteamId, "VENTA.GG");
         
         return @event.DelayedFor(TimeSpan.FromSeconds(120));

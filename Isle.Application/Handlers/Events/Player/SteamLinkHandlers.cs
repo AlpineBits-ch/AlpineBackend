c68a5dae705eb8f91e1b1b942ce77924d7ed8c96
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-21 18:26:33 +0200

    feat: if player not found on link we just register it directly

--- Isle.Application/Handlers/Events/Player/SteamLinkHandlers.cs
+++ Isle.Application/Handlers/Events/Player/SteamLinkHandlers.cs
@@ -1,17 +1,29 @@
 ﻿using Identity.Contracts.Bus.Events;
+using Isle.Contracts.Commands;
 using Isle.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
-namespace Isle.Api.Handlers.Events;
+namespace Isle.Api.Handlers.Events.Player;
 
 public class SteamLinkHandler(MicroserviceContext context, ILogger<SteamLinkHandler> logger)
 {
-    public async Task Handle(SteamLinkedEvent @event)
+    public async Task<CreatePlayerCommand?> Handle(SteamLinkedEvent @event)
     {
 
         var player = await context.Players.FirstOrDefaultAsync(p => p.SteamId == @event.SteamId);
         player?.LinkUserId(@event.UserId);
-        
+
+        if (player is null)
+        {
+            // we just issue a register command to register the player right away with the steam and userId
+            return new CreatePlayerCommand()
+            {
+                SteamId = @event.SteamId,
+                UserId = @event.UserId
+            };
+        }
+
+        return null;
     }
     public async Task Handle(SteamUnlinkedEvent @event)
     {

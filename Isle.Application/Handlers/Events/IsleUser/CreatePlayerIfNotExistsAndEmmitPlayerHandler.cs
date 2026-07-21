29f9570917a28e7be6110cd32c5c6ccb250e0686
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-21 18:40:53 +0200

    fix: connect flow

---
+++ Isle.Application/Handlers/Events/IsleUser/CreatePlayerIfNotExistsAndEmmitPlayerHandler.cs
@@ -0,0 +1,37 @@
+﻿using Isle.Contracts.Commands;
+using Isle.Contracts.Events.Player;
+using Isle.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Wolverine;
+
+namespace Isle.Api.Handlers.Events.IsleUser;
+
+public class CreatePlayerIfNotExistsAndEmmitPlayerHandler
+{
+    public async Task<object?> Handle(UserJoinedIsleServerEvent @event, MicroserviceContext context, IMessageBus bus)
+    {
+        var existingPlayer = context.Players.AsNoTracking().FirstOrDefault(x => x.SteamId == @event.SteamId);
+        
+        if(existingPlayer is null)
+        {
+            
+            await bus.InvokeAsync(new CreatePlayerCommand()
+            {
+                SteamId = @event.SteamId
+            });
+
+            existingPlayer = await context.Players.FirstOrDefaultAsync(x => x.SteamId == @event.SteamId);
+            if (existingPlayer == null)
+            {
+                throw new Exception("Player not found after creation");
+            }
+
+        }
+        return new PlayerConnectedEvent()
+        {
+            SteamId = @event.SteamId,
+            UserId = existingPlayer.UserId,
+            PlayerId = existingPlayer.Id
+        };
+    }
+}

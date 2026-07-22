9c926b25a1521f7ff8685cde217b1c8d2d641d07
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-22 07:44:37 +0200

    feat: add in game link name command

---
+++ Isle.Application/Chat/CommandController/Commands/LinkInGameName.cs
@@ -0,0 +1,29 @@
+﻿using Isle.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Isle.Api.Chat.CommandController.Commands;
+
+public class LinkInGameName(MicroserviceContext microserviceContext) : ChatCommand
+{
+    public override async Task<string> ExecuteAsync(CommandContext context)
+    {
+        if (string.IsNullOrWhiteSpace(context.PlayerName))
+        {
+            return "There was an issue linking your in-game name (400), please report that on our discord!";
+        }
+        
+        var player = await microserviceContext.Players.FirstOrDefaultAsync(p => p.Id == context.PlayerId);
+        if (player == null)
+        {
+            return "There was an issue linking your in-game name (404), please report that on our discord!";
+        }
+        
+        player.InGameName = context.PlayerName;
+        await microserviceContext.SaveChangesAsync();
+        return $"Your in-game name has been linked! Friendly id: {player.Id}, in-game name: {player.InGameName}";
+    }
+
+    public override string Name { get; } = "link";
+    public override string Description { get; } = "Links your in-game name to your account";
+    public override bool IsAdminOnly { get; set; }
+}

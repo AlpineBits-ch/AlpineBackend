1204408957e01cc9392b02c53f92bc3280a257df
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-24 17:54:21 +0200

    chore: add stub handler for kill player

---
+++ Isle.Application/Handlers/Events/Player/PlayerKillEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Isle.Contracts.Events.Player;
+
+namespace Isle.Api.Handlers.Events.Player;
+
+public class PlayerKillEventHandler
+{
+    public static void Handle(PlayerKillEvent @event, ILogger<PlayerKillEventHandler> logger)
+    {
+        logger.LogInformation("Player {PlayerId} killed {KilledPlayerId}", @event.KilerId, @event.VictimId);
+    }
+}

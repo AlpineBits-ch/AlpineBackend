4be355059a7fd3f58070bd7cddd69e0d30f3ce91
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-24 17:35:51 +0200

    feat: add killfeed event

---
+++ Isle.Contracts/Events/Player/PlayerKillEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Isle.Contracts.Events.Player;
+
+public class PlayerKillEvent
+{
+    public string KilerId { get; set; }
+    public string VictimId { get; set; }
+    public double VictimWeightInKg { get; set; }
+}

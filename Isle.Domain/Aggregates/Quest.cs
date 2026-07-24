99c9206b851d93874c46fd7bd9380aecb3d0354b
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-24 11:10:08 +0200

    feat: add reward data and quest system data

---
+++ Isle.Domain/Aggregates/Quest.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Isle.Domain.Entity;
+using Isle.Domain.ValueObjects;
+using Persistence;
+
+namespace Isle.Domain.Aggregates;
+
+public class Quest : Aggregate<Quest>, IPrefixedEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public static string Prefix { get; } = "quest";
+    
+    public ICollection<RewardConfig> Rewards { get; } = new List<RewardConfig>();
+    
+    
+    public ICollection<QuestLocation> Locations { get; } = new List<QuestLocation>();
+    
+}

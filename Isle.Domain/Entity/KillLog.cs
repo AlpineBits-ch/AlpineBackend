bbb47f7526f29c615e9e6bdb1a3ba10fc0096096
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-25 09:24:03 +0200

    feat: add kill log data

---
+++ Isle.Domain/Entity/KillLog.cs
@@ -0,0 +1,14 @@
+﻿using Isle.Domain.Aggregates;
+using Persistence;
+
+namespace Isle.Domain.Entity;
+
+public class KillLog : BaseEntity<KillLog>, IPrefixedEntity
+{
+    public string? KillerId { get; set; }
+    public virtual Player? Killer { get; set; }
+    public string? VictimId { get; set; }
+    public virtual Player? Victim { get; set; }
+    public int VictimWeightKg { get; set; }
+    public static string Prefix { get; } = "Kill_log";
+}

57b7f02458846345647571a2e666b5ee98d76cf0
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-25 15:55:52 +0200

    feat: add channel validator and error response

---
+++ Guild.Domain/Validators/ChannelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Guild.Domain.Aggregates;
+
+namespace Guild.Domain.Validators;
+
+public class ChannelValidator : AbstractValidator<Channel>
+{
+    public ChannelValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty()
+            .Must(v => !v.Any(Char.IsWhiteSpace))
+            .WithMessage("Channel name cannot contain whitespace");
+    }
+}

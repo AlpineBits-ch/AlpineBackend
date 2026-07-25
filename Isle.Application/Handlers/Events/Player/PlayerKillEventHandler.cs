02878efbe35368609063e55597f5466475b86d7b
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-25 09:26:52 +0200

    fix: await add async

--- Isle.Application/Handlers/Events/Player/PlayerKillEventHandler.cs
+++ Isle.Application/Handlers/Events/Player/PlayerKillEventHandler.cs
@@ -6,12 +6,12 @@ namespace Isle.Api.Handlers.Events.Player;
 
 public class PlayerKillEventHandler
 {
-    public static void Handle(PlayerKillEvent @event, ILogger<PlayerKillEventHandler> logger, MicroserviceContext context)
+    public static async Task Handle(PlayerKillEvent @event, ILogger<PlayerKillEventHandler> logger, MicroserviceContext context)
     {
         logger.LogInformation("Player {PlayerId} killed {KilledPlayerId}", @event.KilerId, @event.VictimId);
 
 
-        context.KillLogs.AddAsync(new KillLog()
+        await context.KillLogs.AddAsync(new KillLog()
         {
             Id = KillLog.GenerateId(),
             KillerId = @event.KilerId,

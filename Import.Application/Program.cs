e801b5e7a75798db75dd6c4718ab0bbd9352f362
Dominic Järmann <dominic.jaermann@golog.ch>
2026-07-28 12:28:15 +0200

    fix(import): health check

--- Import.Application/Program.cs
+++ Import.Application/Program.cs
@@ -89,7 +89,7 @@ var app = builder.Build();
 app.UseInfrastructure();
 app.UseGracefulShutdownHealthCheck();
 
-app.MapHealthChecks("/imports/health");
+app.MapHealthChecks("/import/health");
 
 if (app.Environment.IsDevelopment())
 {

92a310286515dc708b9e06e79f820b406fae98bd
Dominic Järmann <dominic.jaermann@golog.ch>
2026-08-06 20:18:07 +0200

    fix(messaging): migrate the message_type enum for call history

--- Messaging.Infrastructure/Migrations/20260806181634_AddCallHistoryMessageTypes.Designer.cs
+++ Messaging.Infrastructure/Migrations/20260806181634_AddCallHistoryMessageTypes.Designer.cs
@@ -5,18 +5,21 @@ using Messaging.Domain.Enums;
 using Messaging.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
 
 #nullable disable
 
-namespace Messaging.Persistence.Migrations
+namespace Messaging.Infrastructure.Migrations
 {
     [DbContext(typeof(MicroserviceContext))]
-    partial class MicroserviceContextModelSnapshot : ModelSnapshot
+    [Migration("20260806181634_AddCallHistoryMessageTypes")]
+    partial class AddCallHistoryMessageTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
